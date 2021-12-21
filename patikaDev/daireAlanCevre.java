81fbbec6cb615af7d0ebd16e7086624c0a3e0641
gozdece <gozdecengiiz98@gmail.com>
2021-12-21 13:00:24 +0300

    commit

--- patikaDev/daireAlanCevre.java
+++ patikaDev/daireAlanCevre.java
@@ -6,14 +6,17 @@ public class daireAlanCevre {
     public static void main(String[] args) {
         
         Scanner inp = new Scanner(System.in);
-        
+
+        System.out.print("Dairenin yarıçap uzunluğunu girin:");
         int r = inp.nextInt();
+
+        System.out.print("Dairenin merkez açısını girin:");
+        int aci = inp.nextInt();
+
         double pi =3.14;
 
-        double cevre = 2 * pi * r;
-        double alan = pi * r * r;
+        double alan = (pi * r * r * aci)/360;
 
-        System.out.println("Dairenin çevresi : "+cevre);
         System.out.println("Dairenin alanı : "+alan);
     }
     
