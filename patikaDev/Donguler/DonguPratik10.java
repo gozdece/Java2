d3c9875d620d4c8b330bd9c36f6bb6f057585f64
gozdece <gozdecengiiz98@gmail.com>
2021-12-28 11:31:41 +0300

    commit

---
+++ patikaDev/Donguler/DonguPratik10.java
@@ -0,0 +1,43 @@
+package patikaDev.Donguler;
+
+import java.util.Scanner;
+
+public class DonguPratik10 {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin:");
+        int numberOne = inp.nextInt();
+
+        System.out.print("Bir sayı girin:");
+        int numberTwo= inp.nextInt();
+
+        int enKucuk;
+        if(numberOne>numberTwo)
+        {
+            enKucuk=numberTwo;
+        }else{
+            enKucuk=numberOne;
+        }
+
+        for(int i=enKucuk; i>1; i--)
+        {
+            if(numberOne%i==0 && numberTwo%i==0)
+            {
+                System.out.println("Ebob:"+i);
+                break;
+            }
+        }
+
+        for(int i=1; i<=numberOne*numberTwo; i++)
+        {
+            if(i%numberOne==0 && i%numberTwo==0)
+            {
+                System.out.println("Ekok:"+i);
+                break;
+            }
+        }
+    }
+    
+}
